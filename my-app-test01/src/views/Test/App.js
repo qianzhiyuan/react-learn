6087537df6547dc6e54b0a17a8997163fb9e990a
garvey <liujiawei@phone580.com>
2020-11-01 18:34:35 +0800

    doc(all): 添加01代码demo

---
+++ my-app-test01/src/views/Test/App.js
@@ -0,0 +1,25 @@
+import logo from '../../assets/img/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React，从现在开始
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
